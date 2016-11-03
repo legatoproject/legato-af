9b5976d24d19661c273120b26c2e378b6a9acb9e
Christophe Gottheimer <cgottheimer@sierrawireless.com>
2016-11-03 16:19:16 +0100

    Protect media streams with wake lock
    Remove le_pm stubbed declaration from audioUnitTest/interface.h
    
    Resolves: LE-2435
    Change-Id: Ib492a41707bd611026b54996365695ca7a7b8248

--- apps/test/audio/audioUnitTest/interfaces.h
+++ apps/test/audio/audioUnitTest/interfaces.h
@@ -1,4 +1,5 @@
 #include "le_audio_interface.h"
+#include "le_pm_interface.h"
 
 #undef LE_KILL_CLIENT
 #define LE_KILL_CLIENT LE_WARN
@@ -36,62 +37,3 @@ le_msg_SessionEventHandlerRef_t myAddServiceCloseHandler
     le_msg_SessionEventHandler_t    handlerFunc,///< [in] Handler function.
     void*                           contextPtr  ///< [in] Opaque pointer value to pass to handler.
 );
-
-//--------------------------------------------------------------------------------------------------
-/**
- * Option LE_PM_REF_COUNT to manage a reference counted wakeup source
- */
-//--------------------------------------------------------------------------------------------------
-#define LE_PM_REF_COUNT 1
-
-
-//--------------------------------------------------------------------------------------------------
-/**
- * Reference to wakeup source used by StayAwake and Relax function
- */
-//--------------------------------------------------------------------------------------------------
-typedef void* le_pm_WakeupSourceRef_t;
-
-//--------------------------------------------------------------------------------------------------
-/**
- * Create a wakeup source  (STUBBED FUNCTION)
- *
- * @return
- *      - Reference to wakeup source (to be used later for acquiring/releasing)
- *
- * @note The process exits if an invalid or existing tag is passed
- */
-//--------------------------------------------------------------------------------------------------
-le_pm_WakeupSourceRef_t le_pm_NewWakeupSource
-(
-    uint32_t createOpts,
-        ///< [IN] Wakeup source options
-
-    const char* wsTag
-        ///< [IN] Context-specific wakeup source tag
-);
-
-//--------------------------------------------------------------------------------------------------
-/**
- * Acquire a wakeup source  (STUBBED FUNCTION)
- *
- */
-//--------------------------------------------------------------------------------------------------
-void le_pm_StayAwake
-(
-    le_pm_WakeupSourceRef_t wsRef
-        ///< [IN] Reference to a created wakeup source
-);
-
-//--------------------------------------------------------------------------------------------------
-/**
- * Release a previously acquired wakeup source  (STUBBED FUNCTION)
- *
- */
-//--------------------------------------------------------------------------------------------------
-void le_pm_Relax
-(
-    le_pm_WakeupSourceRef_t wsRef
-        ///< [IN] Reference to a created wakeup source
-);
-
