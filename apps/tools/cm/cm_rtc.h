8f4cdb90febc39b55ccaadda6e40759c76b08169
Eric Charon <ECH@sierrawireless.com>
2017-01-09 16:57:59 +0100

    Correct CM tools use of rtc functions
    le_rtc_SetUserTime and le_rtc_GetUserTime are based on GPS epoch time but
    there are use in CM tools with a are mixing of posic epoch time functions.
    A conversion between the two times is applied.
    
    Resolves: LE9XXX-524
    Change-Id: I9fe9db920cc1c78ff0323510d5044d0a5232fe93

--- apps/tools/cm/cm_rtc.h
+++ apps/tools/cm/cm_rtc.h
@@ -11,6 +11,20 @@
 #ifndef CMODEM_RTC_INCLUDE_GUARD
 #define CMODEM_RTC_INCLUDE_GUARD
 
+//--------------------------------------------------------------------------------------------------
+/**
+ *  Delta in seconds between Posix time epoch (01 Jan 1970) and GPS time epoch (06 Jan 1980)
+ *  wihtout counting the leap seconds.
+ *  leap seconds definition:
+ *  UTC and GPS time deviate (on average) every 18 months by one additional second. This is called
+ *  a leap second, introduced in UTC time base, necessary to adjust for changes in the
+ *  earth's rotation. The International Atomic Time (TAI) adds the leap seconds to UTC time.
+ *  To have the Delta in seconds between Posix time epoch TAI (01 Jan 1970) and GPS time epoch
+ *  (06 Jan 1980), do: CM_DELTA_POSIX_TIME_EPOCH_GPS_TIME_EPOCH_IN_SEC + 19
+ */
+//--------------------------------------------------------------------------------------------------
+#define  CM_DELTA_POSIX_TIME_EPOCH_GPS_TIME_EPOCH_IN_SEC   315964800
+
 //-------------------------------------------------------------------------------------------------
 /**
  * Print the RTC help text to stdout.
