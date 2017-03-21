20bbc136a60cc5278eb8ac9315ff9090ac0838f1
KHemmadi <khemmadi@sierrawireless.com>
2017-03-21 11:04:08 +0530

    Add Unit Test skeleton for some API
    Unit Test skeleton for atClient and voiceCallService API
    are created. For DCS, skeleton code already exists.
    
    Resolves: LE-6190
    Change-Id: I83bdbb94a1072715a3290620ba8387d531481b05

---
+++ apps/test/atServices/atClientUnitTest/main.c
@@ -0,0 +1,25 @@
+/**
+ * This module implements the unit tests for AT Client API.
+ *
+ * Copyright (C) Sierra Wireless Inc.
+ *
+ */
+#include "legato.h"
+
+
+//--------------------------------------------------------------------------------------------------
+/**
+ * main of the test
+ *
+ */
+//--------------------------------------------------------------------------------------------------
+COMPONENT_INIT
+{
+    // To reactivate for all DEBUG logs
+    //le_log_SetFilterLevel(LE_LOG_DEBUG);
+
+    LE_INFO("======== START UnitTest of AT CLIENT API ========");
+
+    LE_INFO("======== UnitTest of AT CLIENT API FINISHED ========");
+    exit(0);
+}
---
+++ apps/test/voiceCallService/voiceCallServiceUnitTest/main.c
@@ -0,0 +1,25 @@
+/**
+ * This module implements the unit tests for VOICE CALL API.
+ *
+ * Copyright (C) Sierra Wireless Inc.
+ */
+
+#include "legato.h"
+
+//--------------------------------------------------------------------------------------------------
+/**
+ * main of the test
+ *
+ */
+//--------------------------------------------------------------------------------------------------
+COMPONENT_INIT
+{
+
+    // To reactivate for all DEBUG logs
+    //le_log_SetFilterLevel(LE_LOG_DEBUG);
+
+    LE_INFO("======== START UnitTest of VOICE CALL API ========");
+
+    LE_INFO("======== UnitTest of VOICE CALL API FINISHED ========");
+    exit(0);
+}
