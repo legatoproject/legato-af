b2656bf10bfc8c61d9a25b1ef2a6d5e5981a4b64
Manikanta Beluri <manikantab@sierrawireless.com>
2017-09-13 12:03:01 +0530

    Fix Coverity issue for return value check
    Resolves: LE-7372
    Change-Id: I6cc349e91706f781cc62024e52359d52008fac54

--- apps/sample/legacy/useLegatoHandlers/main.c
+++ apps/sample/legacy/useLegatoHandlers/main.c
@@ -10,8 +10,32 @@ static void SmsRxHandler
 )
 {
     char textBuffer[1024];
+    le_result_t res;
 
-    le_sms_GetSenderTel(msg, textBuffer, sizeof(textBuffer));
+    res = le_sms_GetSenderTel(msg, textBuffer, sizeof(textBuffer));
+
+    switch (res)
+    {
+        case LE_OK:
+            LE_INFO("The function succeeded");
+            break;
+
+        case LE_NOT_FOUND:
+            LE_EMERG("Invalid reference (%p) provided!", msg);
+            break;
+
+        case LE_NOT_PERMITTED:
+            LE_ERROR("The message is not a received message");
+            break;
+
+        case LE_FAULT:
+            LE_EMERG("textBuffer is NULL!");
+            break;
+
+        default:
+            LE_EMERG("The textBuffer length exceed the maximum length");
+            break;
+    }
 
     switch (le_sms_GetFormat(msg))
     {
