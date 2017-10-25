d3fb76ed70fd746c1d7dc34bce7a94fca5446f54
Pinkesh Shah <pshah@sierrawireless.com>
2017-10-25 13:56:56 +0530

    Develop a port service stub code
    Resolves: LE-8431
    Change-Id: I714739c81b533dbe3efc998c2fe0bfa57e4a439d

---
+++ apps/test/portServices/portServiceIntegrationTest/portServiceTestComp/portServiceTest.c
@@ -0,0 +1,331 @@
+ /**
+  * This module implements the le_port's tests.
+  *
+  *
+  * Copyright (C) Sierra Wireless Inc.
+  *
+  */
+
+#include "legato.h"
+#include "interfaces.h"
+
+//--------------------------------------------------------------------------------------------------
+/**
+ * Byte length to read from fd.
+ */
+//--------------------------------------------------------------------------------------------------
+#define READ_BYTES 100
+
+//--------------------------------------------------------------------------------------------------
+/**
+ * AtCmd_t definition
+ */
+//--------------------------------------------------------------------------------------------------
+typedef struct
+{
+    const char*                         atCmdPtr;
+    le_atServer_CmdRef_t                cmdRef;
+    le_atServer_CommandHandlerFunc_t    handlerPtr;
+    void*                               contextPtr;
+}
+AtCmd_t;
+
+//--------------------------------------------------------------------------------------------------
+/**
+ * AT Commands definition
+ */
+//--------------------------------------------------------------------------------------------------
+static AtCmd_t AtCmdCreation;
+
+//--------------------------------------------------------------------------------------------------
+/**
+ * Thread and semaphore reference.
+ */
+//--------------------------------------------------------------------------------------------------
+static le_sem_Ref_t Semaphore;
+static le_thread_Ref_t AppThreadRef;
+
+//--------------------------------------------------------------------------------------------------
+/**
+ * Device reference.
+ */
+//--------------------------------------------------------------------------------------------------
+static le_port_DeviceRef_t devRef;
+
+//--------------------------------------------------------------------------------------------------
+/**
+ * Prepare handler.
+ */
+//--------------------------------------------------------------------------------------------------
+static void PrepareHandler
+(
+    le_atServer_CmdRef_t commandRef,
+    le_atServer_Type_t   type,
+    uint32_t             parametersNumber,
+    void*                contextPtr
+)
+{
+    char rsp[LE_ATDEFS_RESPONSE_MAX_BYTES];
+    char atCommandName[LE_ATDEFS_COMMAND_MAX_BYTES];
+    char param[LE_ATDEFS_PARAMETER_MAX_BYTES];
+    int i = 0;
+
+    LE_INFO("commandRef %p", commandRef);
+
+    memset(atCommandName, 0, LE_ATDEFS_COMMAND_MAX_BYTES);
+
+    // Get command name
+    LE_ASSERT_OK(le_atServer_GetCommandName(commandRef, atCommandName,
+                 LE_ATDEFS_COMMAND_MAX_BYTES));
+
+    LE_INFO("AT command name %s", atCommandName);
+
+    memset(rsp, 0, LE_ATDEFS_RESPONSE_MAX_BYTES);
+    snprintf(rsp, LE_ATDEFS_RESPONSE_MAX_BYTES, "%s TYPE: ", atCommandName + 2);
+
+    switch (type)
+    {
+        case LE_ATSERVER_TYPE_PARA:
+            LE_INFO("Type PARA");
+            snprintf(rsp + strlen(rsp), LE_ATDEFS_RESPONSE_MAX_BYTES-strlen(rsp), "PARA");
+        break;
+
+        case LE_ATSERVER_TYPE_TEST:
+            LE_INFO("Type TEST");
+            snprintf(rsp + strlen(rsp), LE_ATDEFS_RESPONSE_MAX_BYTES-strlen(rsp), "TEST");
+        break;
+
+        case LE_ATSERVER_TYPE_READ:
+            LE_INFO("Type READ");
+            snprintf(rsp + strlen(rsp), LE_ATDEFS_RESPONSE_MAX_BYTES-strlen(rsp), "READ");
+        break;
+
+        case LE_ATSERVER_TYPE_ACT:
+            LE_INFO("Type ACT");
+            snprintf(rsp + strlen(rsp), LE_ATDEFS_RESPONSE_MAX_BYTES-strlen(rsp), "ACT");
+        break;
+
+        default:
+            exit(EXIT_FAILURE);
+        break;
+    }
+
+    // Send the command type into an intermediate response
+    LE_ASSERT_OK(le_atServer_SendIntermediateResponse(commandRef, rsp));
+
+    // Send parameters into an intermediate response
+    for (i = 0; i < parametersNumber; i++)
+    {
+        memset(param, 0, LE_ATDEFS_PARAMETER_MAX_BYTES);
+        LE_ASSERT_OK(le_atServer_GetParameter(commandRef,
+                                           i,
+                                           param,
+                                           LE_ATDEFS_PARAMETER_MAX_BYTES));
+
+        memset(rsp, 0, LE_ATDEFS_RESPONSE_MAX_BYTES);
+        snprintf(rsp, LE_ATDEFS_RESPONSE_MAX_BYTES, "%s PARAM %d: %s", atCommandName + 2, i, param);
+
+        LE_ASSERT_OK(le_atServer_SendIntermediateResponse(commandRef, rsp));
+    }
+}
+
+//--------------------------------------------------------------------------------------------------
+/**
+ * AT command Mode handler
+ */
+//--------------------------------------------------------------------------------------------------
+static void AtCmdModeHandler
+(
+    le_atServer_CmdRef_t commandRef,
+    le_atServer_Type_t   type,
+    uint32_t             parametersNumber,
+    void*                contextPtr
+)
+{
+    PrepareHandler(commandRef, type, parametersNumber, contextPtr);
+
+    // Send Final response
+    LE_ASSERT_OK(le_atServer_SendFinalResponse(commandRef, LE_ATSERVER_OK, false, ""));
+
+    le_sem_Post(Semaphore);
+}
+
+//--------------------------------------------------------------------------------------------------
+/**
+ * Data Mode handler
+ */
+//--------------------------------------------------------------------------------------------------
+static void DataModeHandler
+(
+    le_atServer_CmdRef_t commandRef,
+    le_atServer_Type_t   type,
+    uint32_t             parametersNumber,
+    void*                contextPtr
+)
+{
+    int fd;
+    le_result_t result;
+    char buffer[100];
+    ssize_t len;
+    size_t count;
+
+    le_port_ConnectService();
+
+    PrepareHandler(commandRef, type, parametersNumber, contextPtr);
+
+    // Send Final response
+    LE_ASSERT_OK(le_atServer_SendFinalResponse(commandRef, LE_ATSERVER_OK, false, ""));
+
+    result = le_port_SetDataMode(devRef, &fd);
+    if (LE_OK == result)
+    {
+        LE_INFO("fd from port service is %d. le_port_SetDataMode() API success...", fd);
+    }
+    else
+    {
+        LE_ERROR("le_port_SetDataMode() API fails !");
+        exit(EXIT_FAILURE);
+    }
+
+    le_utf8_Copy(buffer, "Test data mode", sizeof(buffer), NULL);
+
+    count = strlen(buffer);
+
+    len = write(fd, buffer, count);
+    if (len == -1)
+    {
+        LE_ERROR("Failed to write to fd %m");
+        le_tty_Close(fd);
+        le_port_Release(devRef);
+        exit(EXIT_FAILURE);
+    }
+    else if (len == count)
+    {
+        LE_INFO("Data is successfully written into device");
+    }
+    else
+    {
+        LE_ERROR("Failed to write data");
+        le_tty_Close(fd);
+        le_port_Release(devRef);
+        exit(EXIT_FAILURE);
+    }
+
+    do
+    {
+        len = read(fd, buffer, READ_BYTES);
+        if (len == -1)
+        {
+            LE_ERROR("Failed to read from fd %m");
+            le_tty_Close(fd);
+            le_port_Release(devRef);
+            exit(EXIT_FAILURE);
+        }
+        if (strstr(buffer, "+++") != NULL)
+        {
+            LE_INFO("Device will switch into AT command mode");
+            break;
+        }
+    } while (1);
+
+    le_sem_Post(Semaphore);
+}
+
+//--------------------------------------------------------------------------------------------------
+/**
+ * Add AtServer handler
+ */
+//--------------------------------------------------------------------------------------------------
+static void* ATServerAddHandler
+(
+    void* contexPtr
+)
+{
+    le_atServer_ConnectService();
+
+    le_atServer_AddCommandHandler(AtCmdCreation.cmdRef,
+                                  AtCmdCreation.handlerPtr,
+                                  AtCmdCreation.contextPtr);
+    le_event_RunLoop();
+}
+
+//--------------------------------------------------------------------------------------------------
+/**
+ * Main of the test. This test code has been written to test the port service stub code APIs.
+ */
+//--------------------------------------------------------------------------------------------------
+COMPONENT_INIT
+{
+    le_result_t result;
+
+    Semaphore = le_sem_Create("HandlerSem",0);
+
+    AtCmdCreation.atCmdPtr = "AT+TESTCMDMODE";
+
+    AtCmdCreation.cmdRef = le_atServer_Create(AtCmdCreation.atCmdPtr);
+    LE_ASSERT(AtCmdCreation.cmdRef != NULL);
+
+    AtCmdCreation.handlerPtr = AtCmdModeHandler;
+    AtCmdCreation.contextPtr = NULL;
+
+    AppThreadRef = le_thread_Create("ATServerHandler", ATServerAddHandler, NULL);
+
+    le_thread_Start(AppThreadRef);
+
+    devRef = le_port_Request("uart");
+    if (NULL != devRef)
+    {
+        LE_INFO("le_port_Request() API success...");
+    }
+    else
+    {
+        LE_ERROR("Device reference is NULL ! le_port_Request() API fails !");
+        exit(EXIT_FAILURE);
+    }
+
+    // Wait untill AT command not received on uart.
+    le_sem_Wait(Semaphore);
+    le_thread_Cancel(AppThreadRef);
+
+    AtCmdCreation.atCmdPtr = "AT+TESTDATAMODE";
+
+    AtCmdCreation.cmdRef = le_atServer_Create(AtCmdCreation.atCmdPtr);
+    LE_ASSERT(AtCmdCreation.cmdRef != NULL);
+
+    AtCmdCreation.handlerPtr = DataModeHandler;
+    AtCmdCreation.contextPtr = NULL;
+
+    AppThreadRef = le_thread_Create("ATServerHandler", ATServerAddHandler, NULL);
+
+    le_thread_Start(AppThreadRef);
+
+    // Wait untill data mode testing completes.
+    le_sem_Wait(Semaphore);
+    le_thread_Cancel(AppThreadRef);
+
+    result = le_port_SetCommandMode(devRef);
+    if (LE_OK == result)
+    {
+        LE_INFO("le_port_SetCommandMode() API success...");
+    }
+    else
+    {
+        LE_ERROR("le_port_SetCommandMode() API fails !");
+        exit(EXIT_FAILURE);
+    }
+
+    result = le_port_Release(devRef);
+    if (LE_OK == result)
+    {
+        LE_INFO("le_port_Release() API success...");
+    }
+    else
+    {
+        LE_ERROR("le_port_Release() API fails !");
+        exit(EXIT_FAILURE);
+    }
+
+    LE_INFO("======= Port service Integration Test completes =======");
+
+    exit(EXIT_SUCCESS);
+}
--- components/portService/portDaemon/le_port.c
+++ components/portService/portDaemon/le_port.c
@@ -16,12 +16,154 @@
 
 //--------------------------------------------------------------------------------------------------
 /**
+ * Maximum number of client applications.
+ */
+//--------------------------------------------------------------------------------------------------
+#define MAX_APPS 2
+
+//--------------------------------------------------------------------------------------------------
+/**
+ * Max length for error string
+ */
+//--------------------------------------------------------------------------------------------------
+#define ERR_MSG_MAX 256
+
+//--------------------------------------------------------------------------------------------------
+/**
+ * Device mode flag for blocking / non-blocking.
+ */
+//--------------------------------------------------------------------------------------------------
+#define BLOCKING_MODE     true
+#define NON_BLOCKING_MODE false
+
+//--------------------------------------------------------------------------------------------------
+/**
+ * Client application request object structure.
+ */
+//--------------------------------------------------------------------------------------------------
+typedef struct
+{
+    char                    deviceNamePtr[LE_PORT_MAX_LEN_DEVICE_NAME]; ///< The device name.
+    int32_t                 fd;                                         ///< The device indentifier.
+    int32_t                 dataModeFd;                                 ///< The device indentifier
+                                                                        ///< specific to data mode.
+    le_port_DeviceRef_t     deviceRef;                                  ///< Device reference for
+                                                                        ///< client.
+    le_atServer_DeviceRef_t atServerDevRef;                             ///< AT server device
+                                                                        ///< reference.
+    le_msg_SessionRef_t     sessionRef;                                 ///< Client session
+                                                                        ///< identifier.
+    le_dls_Link_t           link;                                       ///< Object node link.
+}
+DeviceObject_t;
+
+
+//--------------------------------------------------------------------------------------------------
+/**
+ * Memory Pool for Port Service Client Handler.
+ */
+//--------------------------------------------------------------------------------------------------
+static le_mem_PoolRef_t PortPoolRef;
+
+//--------------------------------------------------------------------------------------------------
+/**
+ * Safe Reference Map for service activation requests.
+ */
+//--------------------------------------------------------------------------------------------------
+static le_ref_MapRef_t DevicesRefMap;
+
+//--------------------------------------------------------------------------------------------------
+/**
+ * Device object list.
+ */
+//--------------------------------------------------------------------------------------------------
+static le_dls_List_t DeviceList;
+
+
+//--------------------------------------------------------------------------------------------------
+/**
+ * This function opens the device.
+ *
+ * @return
+ *      - -1 if function failed.
+ *      - Valid file descriptor.
+ *
+ * @note This is a stub code to test the port service APIs.
+ */
+//--------------------------------------------------------------------------------------------------
+static int32_t OpenDevice
+(
+    char* deviceName,  ///< [IN] File descriptor.
+    bool blokingMode   ///< [IN] Device blocking mode. 0 -> non-blocking mode, 1 -> blocking mode.
+)
+{
+    int32_t fd;
+
+    fd = le_tty_Open(deviceName, O_RDWR | O_NOCTTY);
+    if (-1 == fd)
+    {
+        LE_ERROR("Failed to open device");
+        return -1;
+    }
+
+    if (LE_OK != le_tty_SetBaudRate(fd, LE_TTY_SPEED_115200))
+    {
+        LE_ERROR("Failed to configure TTY baud rate");
+        goto error;
+    }
+
+    if (LE_OK != le_tty_SetFraming(fd, 'N', 8, 1))
+    {
+        LE_ERROR("Failed to configure TTY framing");
+        goto error;
+    }
+
+    if (LE_OK != le_tty_SetFlowControl(fd, LE_TTY_FLOW_CONTROL_NONE))
+    {
+        LE_ERROR("Failed to configure TTY flow control");
+        goto error;
+    }
+
+    if (blokingMode == false)
+    {
+        // Set serial port into raw (non-canonical) mode. Disables conversion of EOL characters,
+        // disables local echo, numChars = 0 and timeout = 0: Read will be completetly non-blocking.
+        if (LE_OK != le_tty_SetRaw(fd, 0, 0))
+        {
+            LE_ERROR("Failed to configure TTY raw");
+            goto error;
+        }
+    }
+    else
+    {
+        // Set serial port into raw (non-canonical) mode. Disables conversion of EOL characters,
+        // disables local echo, numChars = 50 and timeout = 50: Read will be completetly blocking.
+        if (LE_OK != le_tty_SetRaw(fd, 50, 50))
+        {
+            LE_ERROR("Failed to configure TTY raw");
+            goto error;
+        }
+    }
+
+    return fd;
+
+error:
+    // Close the TTY
+    le_tty_Close(fd);
+
+    return -1;
+}
+
+//--------------------------------------------------------------------------------------------------
+/**
  * This function requests to open a configured device. If the device was not opened, it opens
  * the device.
  *
  * @return
  *      - Reference to the device.
  *      - NULL if the device is not available.
+ *
+ * @note This is a stub code to test the port service APIs.
  */
 //--------------------------------------------------------------------------------------------------
 le_port_DeviceRef_t le_port_Request
@@ -29,7 +171,43 @@ le_port_DeviceRef_t le_port_Request
     const char* deviceNamePtr  ///< [IN] Device name to be requested.
 )
 {
-    return NULL;
+    if ((NULL == deviceNamePtr) || !(strcmp(deviceNamePtr, "")))
+    {
+        LE_ERROR("deviceNamePtr is not valid !");
+        return NULL;
+    }
+
+    DeviceObject_t* deviceObjectPtr = le_mem_ForceAlloc(PortPoolRef);
+
+    // Initialize the data mode file descriptor.
+    deviceObjectPtr->dataModeFd = 0;
+
+    le_port_DeviceRef_t devRef = le_ref_CreateRef(DevicesRefMap, deviceObjectPtr);
+    if (NULL == devRef)
+    {
+        LE_ERROR("devRef is NULL !");
+        return NULL;
+    }
+
+    // Save the device name.
+    le_utf8_Copy(deviceObjectPtr->deviceNamePtr, "/dev/ttyHSL1", LE_PORT_MAX_LEN_DEVICE_NAME, NULL);
+
+    // Save the client session.
+    deviceObjectPtr->sessionRef = le_port_GetClientSessionRef();
+    deviceObjectPtr->deviceRef = devRef;
+
+    // Open the device (for example: /dev/ttyHSL1)
+    deviceObjectPtr->fd = OpenDevice("/dev/ttyHSL1", NON_BLOCKING_MODE);
+    if (0 > deviceObjectPtr->fd)
+    {
+        LE_ERROR("Error in opening the device '%s': %m", deviceNamePtr);
+        return NULL;
+    }
+
+    deviceObjectPtr->atServerDevRef = le_atServer_Open(deviceObjectPtr->fd);
+    le_dls_Queue(&DeviceList, &(deviceObjectPtr->link));
+
+    return devRef;
 }
 
 //--------------------------------------------------------------------------------------------------
@@ -37,17 +215,54 @@ le_port_DeviceRef_t le_port_Request
  * This function switches the device into data mode.
  *
  * @return
- *      - LE_OK    Function succeeded.
- *      - LE_FAULT Function failed.
+ *      - LE_OK            Function succeeded.
+ *      - LE_FAULT         Function failed.
+ *      - LE_BAD_PARAMETER Invalid parameter.
+ *
+ * @note This is a stub code to test the port service APIs.
  */
 //--------------------------------------------------------------------------------------------------
 le_result_t le_port_SetDataMode
 (
     le_port_DeviceRef_t devRef,   ///< [IN] Device reference.
-    int8_t* fdPtr                 ///< [OUT] File descriptor of the device.
+    int32_t* fdPtr                ///< [OUT] File descriptor of the device.
 )
 {
-    return LE_OK;
+    le_result_t result;
+
+    DeviceObject_t* deviceObjectPtr = le_ref_Lookup(DevicesRefMap, devRef);
+    if (NULL == deviceObjectPtr)
+    {
+        LE_ERROR("devRef is invalid !");
+        return LE_BAD_PARAMETER;
+    }
+
+    if (NULL == fdPtr)
+    {
+        LE_ERROR("fdPtr is NULL !");
+        return LE_BAD_PARAMETER;
+    }
+
+    le_atServer_DeviceRef_t atServerDeviceRef = deviceObjectPtr->atServerDevRef;
+    result = le_atServer_Suspend(atServerDeviceRef);
+    if (LE_FAULT == result)
+    {
+        LE_ERROR("Device is not able to switch into data mode");
+        return LE_FAULT;
+    }
+
+    deviceObjectPtr->dataModeFd = OpenDevice(deviceObjectPtr->deviceNamePtr, BLOCKING_MODE);
+
+    if (deviceObjectPtr->dataModeFd != -1)
+    {
+        *fdPtr = (deviceObjectPtr->dataModeFd);
+        return LE_OK;
+    }
+    else
+    {
+        *fdPtr = -1;
+        return LE_FAULT;
+    }
 }
 
 //--------------------------------------------------------------------------------------------------
@@ -55,8 +270,11 @@ le_result_t le_port_SetDataMode
  * This function switches the device into AT command mode.
  *
  * @return
- *      - LE_OK    Function succeeded.
- *      - LE_FAULT Function failed.
+ *      - LE_OK            Function succeeded.
+ *      - LE_FAULT         Function failed.
+ *      - LE_BAD_PARAMETER Invalid parameter.
+ *
+ * @note This is a stub code to test the port service APIs.
  */
 //--------------------------------------------------------------------------------------------------
 le_result_t le_port_SetCommandMode
@@ -64,6 +282,23 @@ le_result_t le_port_SetCommandMode
     le_port_DeviceRef_t devRef    ///< [IN] Device reference.
 )
 {
+    le_result_t result;
+
+    DeviceObject_t* deviceObjectPtr = le_ref_Lookup(DevicesRefMap, devRef);
+    if (NULL == deviceObjectPtr)
+    {
+        LE_ERROR("devRef is invalid !");
+        return LE_BAD_PARAMETER;
+    }
+
+    le_atServer_DeviceRef_t atServerDeviceRef = deviceObjectPtr->atServerDevRef;
+    result = le_atServer_Resume(atServerDeviceRef);
+    if (LE_FAULT == result)
+    {
+        LE_ERROR("Device is not able to switch into command mode");
+        return LE_FAULT;
+    }
+
     return LE_OK;
 }
 
@@ -72,8 +307,11 @@ le_result_t le_port_SetCommandMode
  * This function closes the device and releases the resources.
  *
  * @return
- *      - LE_OK    Function succeeded.
- *      - LE_FAULT Function failed.
+ *      - LE_OK            Function succeeded.
+ *      - LE_FAULT         Function failed.
+ *      - LE_BAD_PARAMETER Invalid parameter.
+ *
+ * @note This is a stub code to test the port service APIs.
  */
 //--------------------------------------------------------------------------------------------------
 le_result_t le_port_Release
@@ -81,6 +319,26 @@ le_result_t le_port_Release
     le_port_DeviceRef_t devRef    ///< [IN] Device reference.
 )
 {
+    le_result_t result;
+
+    DeviceObject_t* deviceObjectPtr = le_ref_Lookup(DevicesRefMap, devRef);
+    if (NULL == deviceObjectPtr)
+    {
+        LE_ERROR("devRef is invalid !");
+        return LE_BAD_PARAMETER;
+    }
+
+    le_atServer_DeviceRef_t atServerDeviceRef = deviceObjectPtr->atServerDevRef;
+    result = le_atServer_Close(atServerDeviceRef);
+    if (LE_FAULT == result)
+    {
+        LE_ERROR("Device is not able to close");
+        return LE_FAULT;
+    }
+
+    le_ref_DeleteRef(DevicesRefMap, devRef);
+    le_mem_Release(deviceObjectPtr);
+
     return LE_OK;
 }
 
@@ -89,8 +347,11 @@ le_result_t le_port_Release
  * This function gets the device reference regarding to a given reference coming from the AT server.
  *
  * @return
- *      - LE_OK    Function succeeded.
- *      - LE_FAULT Function failed.
+ *      - LE_OK            Function succeeded.
+ *      - LE_FAULT         Function failed.
+ *      - LE_BAD_PARAMETER Invalid parameter.
+ *
+ * @note This is a stub code to test the port service APIs.
  */
 //--------------------------------------------------------------------------------------------------
 le_result_t le_port_GetPortReference
@@ -99,7 +360,28 @@ le_result_t le_port_GetPortReference
     le_port_DeviceRef_t* devRefPtr            ///< [OUT] Device reference from port service.
 )
 {
-    return LE_OK;
+    if (NULL == devRefPtr)
+    {
+        LE_ERROR("devRefPtr is NULL !");
+        return LE_BAD_PARAMETER;
+    }
+
+    le_dls_Link_t* linkPtr = le_dls_Peek(&DeviceList);
+
+    while (linkPtr)
+    {
+        DeviceObject_t* deviceObjectPtr = CONTAINER_OF(linkPtr, DeviceObject_t, link);
+        linkPtr = le_dls_PeekNext(&DeviceList, linkPtr);
+
+        if (deviceObjectPtr->atServerDevRef == atServerDevRef)
+        {
+            *devRefPtr = deviceObjectPtr->deviceRef;
+            return LE_OK;
+        }
+    }
+
+    *devRefPtr = NULL;
+    return LE_FAULT;
 }
 
 
@@ -110,5 +392,12 @@ le_result_t le_port_GetPortReference
 //--------------------------------------------------------------------------------------------------
 COMPONENT_INIT
 {
+    // Create safe reference map for request references.
+    DevicesRefMap = le_ref_CreateMap("DeviceRef", MAX_APPS);
 
+    // Create a pool for client objects.
+    PortPoolRef = le_mem_CreatePool("PortPoolRef", sizeof(DeviceObject_t));
+    le_mem_ExpandPool(PortPoolRef, MAX_APPS);
+
+    DeviceList = LE_DLS_LIST_INIT;
 }
