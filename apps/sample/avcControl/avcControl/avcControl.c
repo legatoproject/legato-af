a3ad8db328778e70d581f74946e7fcdb0cf55fb0
Janice Mardjuki <JMardjuki@sierrawireless.com>
2018-02-06 19:20:06 -0800

    AirVantage sample app clean up
    Resolves: LE-9340
    Change-Id: I4cfe04db96e378f44b5fdf5293ada434d2ef6468

--- apps/sample/avcControl/avcControl/avcControl.c
+++ apps/sample/avcControl/avcControl/avcControl.c
@@ -6,8 +6,6 @@
  * Provide an AirVantage control application with the following behaviours:
  * - Automatic download/install of OTA packages
  * - Receive incoming SMS wake up messages
- * - Polling timer
- * - Retry timers
  *
  * <hr>
  *
@@ -19,41 +17,9 @@
 #include "interfaces.h"
 
 
-// -------------------------------------------------------------------------------------------------
-/**
- *  Polling timer reference. Time interval to automatically start an AVC session.
- */
-// ------------------------------------------------------------------------------------------------
-static le_timer_Ref_t PollingTimerRef = NULL;
-
-// -------------------------------------------------------------------------------------------------
-/**
- *  Polling timer interval in minutes. Set to 1 day.
- */
-// ------------------------------------------------------------------------------------------------
-#define POLLING_TIMER_MIN 24*60
-
-//--------------------------------------------------------------------------------------------------
-/**
- * Denoting a session is established to the DM serevr.
- */
-//--------------------------------------------------------------------------------------------------
-static bool SessionStarted = false;
-
-//--------------------------------------------------------------------------------------------------
-/**
- * Retry timers related data. RetryTimersIndex is index to the array of RetryTimers.
- * A timer of value 0 means it's disabled. The timers values are in minutes.
- */
-//--------------------------------------------------------------------------------------------------
-static le_timer_Ref_t RetryTimerRef = NULL;
-static uint16_t RetryTimersIndex = 0;
-#define NUM_RETRY_TIMERS 4
-static uint16_t RetryTimers[NUM_RETRY_TIMERS] = {15, 60, 240, 480};
-
 //-------------------------------------------------------------------------------------------------
 /**
- * Fetch a string describing the type of update underway over Air Vantage.
+ * Fetch a string describing the type of update underway over AirVantage.
  *
  * @return Pointer to a null-terminated string constant.
  */
@@ -174,12 +140,10 @@ static void StatusHandler
 
         case LE_AVC_SESSION_STARTED:
             statusPtr = "SESSION_STARTED";
-            SessionStarted = true;
             break;
 
         case LE_AVC_SESSION_STOPPED:
             statusPtr = "SESSION_STOPPED";
-            SessionStarted = false;
             break;
 
         case LE_AVC_REBOOT_PENDING:
@@ -295,112 +259,13 @@ static void SmsReceivedHandler
     le_sms_DeleteFromStorage(messagePtr);
 }
 
-
-//--------------------------------------------------------------------------------------------------
-/**
- * Reset the retry timers by resetting the RetryTimersIndex, and stopping the current retry timer.
- */
-//--------------------------------------------------------------------------------------------------
-static void ResetRetryTimers
-(
-    void
-)
-{
-    RetryTimersIndex = 0;
-    le_timer_Stop(RetryTimerRef);
-}
-
-
-//-------------------------------------------------------------------------------------------------
-/**
- * Start an AVC session. Retry if session doesn't start.
- */
-//-------------------------------------------------------------------------------------------------
-static void StartSession
-(
-    le_timer_Ref_t timerRef
-)
-{
-    if (SessionStarted)
-    {
-        // No need to start a retry timer. Perform reset/cleanup.
-        ResetRetryTimers();
-    }
-    else
-    {
-        // Retrying. LE_FAULT shouldn't happen because this app is the control app.
-        LE_ASSERT(LE_FAULT != le_avc_StopSession());
-        LE_ASSERT(LE_FAULT != le_avc_StartSession());
-
-        // Increment the index except for the first try.
-        if (RetryTimersIndex != 0)
-        {
-            RetryTimersIndex++;
-        }
-
-        // Attempt to start a retry timer.
-        // See which timer we are at by looking at RetryTimersIndex
-        // if the timer is 0, get the next one. (0 means disabled / not used)
-        // if we run out of timers, do nothing.  Perform reset/cleanup
-        while((RetryTimersIndex < NUM_RETRY_TIMERS) &&
-              (0 == RetryTimers[RetryTimersIndex]))
-        {
-                RetryTimersIndex++;
-        }
-
-        // This is the case when we've run out of timers. Reset/cleanup, and don't start the next
-        // retry timer (since there aren't any left).
-        if (RetryTimersIndex >= NUM_RETRY_TIMERS)
-        {
-            ResetRetryTimers();
-        }
-        // Start the next retry timer.
-        else
-        {
-            le_clk_Time_t interval = {RetryTimers[RetryTimersIndex] * 60, 0};
-
-            LE_ASSERT(LE_OK == le_timer_SetInterval(RetryTimerRef, interval));
-            LE_ASSERT(LE_OK == le_timer_SetHandler(RetryTimerRef, StartSession));
-            LE_ASSERT(LE_OK == le_timer_Start(RetryTimerRef));
-        }
-    }
-}
-
-
-//-------------------------------------------------------------------------------------------------
-/**
- * Start an AVC session periodically.
- */
-//-------------------------------------------------------------------------------------------------
-static void StartPollingTimer
-(
-    void
-)
-{
-    LE_INFO("Polling timer is set to start AVC session every %d minutes.", POLLING_TIMER_MIN);
-
-    le_clk_Time_t interval = {POLLING_TIMER_MIN * 60, 0};
-    PollingTimerRef = le_timer_Create("avcControl PollingTimer");
-
-    LE_ASSERT(LE_OK == le_timer_SetInterval(PollingTimerRef, interval));
-    LE_ASSERT(LE_OK == le_timer_SetRepeat(PollingTimerRef, 0));
-    LE_ASSERT(LE_OK == le_timer_SetHandler(PollingTimerRef, StartSession));
-    LE_ASSERT(LE_OK == le_timer_Start(PollingTimerRef));
-}
-
-
 COMPONENT_INIT
 {
     // Register AirVantage status report handler. This makes this app "control app".
     le_avc_AddStatusEventHandler(StatusHandler, NULL);
 
-    RetryTimerRef = le_timer_Create("avcControl RetryTimer");
-
     // Start an AVC session at least once.
-    StartSession(NULL);
-
-    // Start an AVC session periodically.
-    StartPollingTimer();
+    LE_ASSERT(LE_FAULT != le_avc_StartSession());
 
     // Register sms handler for SMS wakeup
     le_sms_AddRxMessageHandler(SmsReceivedHandler, NULL);
