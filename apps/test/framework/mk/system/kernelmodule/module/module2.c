31d93b580e7e4fc466b74e08c0ec4387df0c6beb
Shristi Pradhan <spradhan@sierrawireless.com>
2018-05-02 22:13:37 -0700

    [kernelModules UT] Add unit tests for kernel modules dependency support
    This patch covers unit tests for kernel modules dependency support.
    
    Resolves: LE-10063
    Change-Id: I0848fb03e8189792e210a163e45de6c593e21299

---
+++ apps/test/framework/mk/system/kernelmodule/module/module2.c
@@ -0,0 +1,30 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+static char *param1 = "PARAM1";
+module_param(param1, charp, S_IRUGO);
+MODULE_PARM_DESC(param1, "First module parameter");
+
+static char *param2 = "PARAM2";
+module_param(param2, charp, S_IRUGO);
+MODULE_PARM_DESC(param2, "Second module parameter");
+
+static int __init module2_init(void)
+{
+   pr_info("Executing %s(), param1='%s' param2='%s'.\n", __func__, param1, param2);
+   return 0;
+}
+
+static void __exit module2_exit(void)
+{
+   pr_info("Executing %s().\n", __func__);
+}
+
+module_init(module2_init);
+module_exit(module2_exit);
+
+MODULE_LICENSE("MPL");
+MODULE_AUTHOR("Sierra Wireless, Inc.");
+MODULE_DESCRIPTION("Legato kernel module for testing");
+MODULE_VERSION("1.0");
