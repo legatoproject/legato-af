8b62dfa1a480a48dff6fe7d98e1682750e0885cc
Frederic DUR <fdur@sierrawireless.com>
2018-08-03 11:27:04 +0200

    Add SIERRA flag for tinyDTLS component
    Resolves: LE-10716 LE-10774
    Related: LE-10754
    Change-Id: Ib67721cf9749a6172048f16b93755e8ba0785442

--- components/3rdParty/tinydtls/make.sh
+++ components/3rdParty/tinydtls/make.sh
@@ -9,7 +9,7 @@ autoreconf -i
 ac_cv_func_malloc_0_nonnull=yes  ./configure \
     --prefix="${LEGATO_BUILD}/3rdParty/tinydtls" \
     --host="$($CC -dumpmachine)" \
-    CFLAGS="-w -std=gnu99 -DWITH_SHA256 -I${LEGATO_ROOT}/3rdParty/Lwm2mCore/include/platform-specific/linux -fPIC ${CFLAGS}" \
+    CFLAGS="-w -std=gnu99 -DSIERRA -DWITH_SHA256 -I${LEGATO_ROOT}/3rdParty/Lwm2mCore/include/platform-specific/linux -fPIC ${CFLAGS}" \
     LDFLAGS="${LDFLAGS}"
 make
 make install
