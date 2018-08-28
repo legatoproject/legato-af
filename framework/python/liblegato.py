9ec7214df38093ba8ed5fbf59d338a2c375bff8e
Laurent Bouinot <LBU@sierrawireless.com>
2018-08-28 13:30:39 +0200

    [python] Impossible to use C char type
    The C code generated by CFFI needs a python string object for the
    initialization of a char (string of size 1), not a cffi object.
    
    Resolves: LE-10967
    Change-Id: I154e8b74f75fe6d5e4f2ece05543a7be2afbd027

--- framework/python/liblegato.py
+++ framework/python/liblegato.py
@@ -45,8 +45,6 @@ def convert_to_c(obj):
         return obj
     if obj is None:
         return ffi.NULL
-    if isinstance(obj, basestring):
-        return ffi.new("char[]", obj)
     if obj is list or obj is tuple:
         return [ convert_to_c[x] for x in obj ]
     if isinstance(obj, Enum):
