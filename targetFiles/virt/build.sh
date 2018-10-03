299c9c5b588f23a2931dd2c5ca72f5ea6aba035d
Bertrand Roussel <broussel@sierrawireless.com>
2018-10-02 23:59:12 -0700

    Make findtoolchain compatible with Yocto 2.5
    With latest version of Yocto, the build tree structure has changed
    and there is no 'global sysroot' anymore.
    
    Instead the sysroots are generated for each package builds.
    
    This adapts the findtoolchain script to be compatible with both,
    by using the sysroot generated for the legato-af package.
    
    I'm also updating the virt script as to be able to generate a Docker
    image with virt from an existing Yocto build.
    
    Resolves: LE-11265
    Change-Id: I2403192b50839817a798894f852bb495d4c38aa6

--- targetFiles/virt/build.sh
+++ targetFiles/virt/build.sh
@@ -2,17 +2,22 @@
 
 set -xe
 
-if [ -z "$LEGATO_ROOT" ]; then
-    echo "LEGATO_ROOT not set"
-    exit 1
-fi
-
-. "$LEGATO_ROOT/framework/tools/scripts/shlib"
-
 SCRIPT_PATH="$( cd `dirname "$0"` && pwd )/`basename $0`"
 if [ -L "$SCRIPT_PATH" ]; then
     SCRIPT_PATH=$(readlink $SCRIPT_PATH)
 fi
+
+if [ -z "$LEGATO_ROOT" ]; then
+    LEGATO_ROOT="$(dirname $SCRIPT_PATH)/../.."
+    if [ ! -e "${LEGATO_ROOT}/framework" ]; then
+        echo "LEGATO_ROOT not set"
+        exit 1
+    fi
+
+    LEGATO_ROOT="$(readlink -f "${LEGATO_ROOT}")"
+fi
+
+. "$LEGATO_ROOT/framework/tools/scripts/shlib"
 
 VIRT_TARGET_ARCH=${VIRT_TARGET_ARCH:-x86}
 
@@ -29,21 +34,49 @@ mkdir -p $BUILD_DIR
 # Configuration Script
 cp "$LEGATO_ROOT/framework/tools/scripts/legato-qemu" "$BUILD_DIR"
 
+if [ -z "$USER_DIR" ]; then
+    export USER_DIR="$BUILD_DIR"
+fi
+
+# Use Yocto & Legato from Yocto build if available
+YOCTO_DIR="$LEGATO_ROOT/../build_virt-${VIRT_TARGET_ARCH}/tmp/deploy/images/swi-virt-${VIRT_TARGET_ARCH}/"
+if [ -e "$YOCTO_DIR" ]; then
+    echo "Yocto build detected"
+
+    TARBALL_PATH="${YOCTO_DIR}/img-virt-${VIRT_TARGET_ARCH}.tar.bz2"
+    if [ ! -e "$TARBALL_PATH" ]; then
+        echo "Yocto not built, $TARBALL_PATH does not exist."
+        exit 1
+    fi
+
+    export LEGATO_IMG="${YOCTO_DIR}/legato-image.virt-${VIRT_TARGET_ARCH}.squashfs"
+    if [ ! -e "$LEGATO_IMG" ]; then
+        echo "Unable to find Legato image as built from Yocto"
+        exit 1
+    fi
+
+    rm -rf "$BUILD_DIR/img-virt-${VIRT_TARGET_ARCH}"
+    mkdir -p "$BUILD_DIR/img-virt-${VIRT_TARGET_ARCH}"
+
+    tar jxvf $TARBALL_PATH -C "$BUILD_DIR/img-virt-${VIRT_TARGET_ARCH}"
+
+    SKIP_DOWNLOAD=1
+fi
+
 # Download Legato image & Linux image
 if [ -z "$SKIP_DOWNLOAD" ]; then
-    export USER_DIR="$BUILD_DIR"
 
     # Use ONLY_PREPARE=1 to prevent the script from starting qemu.
     # Use OPT_PERSIST=0 to prevent the script from generating an empty userfs partition.
     ONLY_PREPARE=1 OPT_PERSIST=0 $BUILD_DIR/legato-qemu
-
-    export KERNEL_IMG="$USER_DIR/img-virt-${VIRT_TARGET_ARCH}/kernel"
-    export ROOTFS_IMG="$USER_DIR/img-virt-${VIRT_TARGET_ARCH}/rootfs.qcow2"
-
-    export LEGATO_IMG="$USER_DIR/img-virt-${VIRT_TARGET_ARCH}/legato.squashfs"
-
-    export QEMU_CONFIG="$USER_DIR/img-virt-${VIRT_TARGET_ARCH}/qemu-config"
 fi
+
+export KERNEL_IMG="${KERNEL_IMG:-"$USER_DIR/img-virt-${VIRT_TARGET_ARCH}/kernel"}"
+export ROOTFS_IMG="${ROOTFS_IMG:-"$USER_DIR/img-virt-${VIRT_TARGET_ARCH}/rootfs.qcow2"}"
+
+export LEGATO_IMG="${LEGATO_IMG:-"$USER_DIR/img-virt-${VIRT_TARGET_ARCH}/legato.squashfs"}"
+
+export QEMU_CONFIG="${QEMU_CONFIG:-"$USER_DIR/img-virt-${VIRT_TARGET_ARCH}/qemu-config"}"
 
 if [ ! -f "$LEGATO_IMG" ]; then
     echo "legato image ($LEGATO_IMG) not available, please build Legato before packaging the Docker image"
@@ -92,32 +125,40 @@ if [ -z "$USERFS_IMG" ] || [ ! -e "$USERFS_IMG" ]; then
     set +e
 fi
 
+# Prepare staging area
+
+rm -rf "$BUILD_DIR/staging"
+mkdir "$BUILD_DIR/staging"
+
+# Script
+cp "$BUILD_DIR/legato-qemu" "$BUILD_DIR/staging/legato-qemu"
+
 # Linux
-cp "$KERNEL_IMG" "$BUILD_DIR"
-cp "$ROOTFS_IMG" "$BUILD_DIR"
+cp "$KERNEL_IMG" "$BUILD_DIR/staging/kernel"
+cp "$ROOTFS_IMG" "$BUILD_DIR/staging/rootfs.qcow2"
 
 # Legato
-cp "$LEGATO_IMG" "$BUILD_DIR"
+cp "$LEGATO_IMG" "$BUILD_DIR/staging/legato.squashfs"
 
 # User FS
-cp "$USERFS_IMG" "$BUILD_DIR"
+cp "$USERFS_IMG" "$BUILD_DIR/staging/userfs.qcow2"
 
 # Config
-cp "$QEMU_CONFIG" "$BUILD_DIR"
+cp "$QEMU_CONFIG" "$BUILD_DIR/staging/qemu-config"
 
-cp $(dirname ${SCRIPT_PATH})/Dockerfile.${VIRT_TARGET_ARCH} ${BUILD_DIR}/Dockerfile
+cp "$(dirname ${SCRIPT_PATH})/Dockerfile.${VIRT_TARGET_ARCH}" "${BUILD_DIR}/staging/Dockerfile"
 
 ## Build image
 
-cd $BUILD_DIR
+cd $BUILD_DIR/staging/
 
-docker build . | tee -a docker-build.log
+docker build . | tee -a ../docker-build.log
 if [ ${PIPESTATUS[0]} -ne 0 ]; then
     echo "Image build failed"
     exit 1
 fi
 
-BUILD_ID=$(tail -1 docker-build.log | awk '{print $3}')
+BUILD_ID=$(tail -1 ../docker-build.log | awk '{print $3}')
 if [ -z "${BUILD_ID}" ] || [ "${#BUILD_ID}" -gt 12 ]; then
     echo "Invalid image ID"
     exit 1
@@ -129,7 +170,7 @@ if [ -z "$LONG_BUILD_ID" ]; then
     exit 1
 fi
 
-echo "$LONG_BUILD_ID" > image.id
+echo "$LONG_BUILD_ID" > ../image.id
 
-docker save -o virt.img "$LONG_BUILD_ID"
+docker save -o ../virt.img "$LONG_BUILD_ID"
 
