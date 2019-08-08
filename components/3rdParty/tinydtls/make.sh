47dedd9f5aeb958cbf5c34a88b78c983331da8d3
Prushothaman Palanichamy <ppalanicha@sierrawireless.com>
2019-08-07 18:28:41 -0700

    Merge tinydtls master to master-swi
    Resolves: LE-13234
    Change-Id: I2132ec36a7fee16ee9c449e6cf5f49f4f5b9e753

--- components/3rdParty/tinydtls/make.sh
+++ components/3rdParty/tinydtls/make.sh
@@ -11,7 +11,10 @@ elif [ x"$LE_CONFIG_CUSTOM_OS" = "xy" ]; then
 fi
 
 cp -rf ${LEGATO_ROOT}/3rdParty/Lwm2mCore/3rdParty/tinydtls/* .
-autoreconf -i
+autoconf
+autoheader
+
+# Configure expects an install file in build directory
 ac_cv_func_malloc_0_nonnull=yes  ./configure \
     --prefix="${LEGATO_BUILD}/3rdParty/tinydtls" \
     --host="$($CC -dumpmachine)" \
