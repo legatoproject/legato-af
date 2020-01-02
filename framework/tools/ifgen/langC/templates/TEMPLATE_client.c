a83c8a8b80fcb0222a5c4cb96147aecba75a59a4
Keith Dunwoody <kdunwoody@sierrawireless.com>
2020-01-02 13:14:06 -0800

    Inline portions of client stubs
    The client stubs have a very small function to add the remote connection
    reference, then a larger common code to handle pack/unpack.  Inline the
    small stub, as the function call overhead for this function was a
    significant portion of the code.
    
    This change saves 16KB of code space on rc76.
    
    Resolves: LE-14044
    Change-Id: Iaffa9da1e7b03390fcaad46c536f6d53dbe320d7

--- framework/tools/ifgen/langC/templates/TEMPLATE_client.c
+++ framework/tools/ifgen/langC/templates/TEMPLATE_client.c
@@ -180,7 +180,7 @@ static _ClientThreadData_t* GetClientThreadDataPtr
  * If the current thread does not have a session ref, then this is a fatal error.
  */
 //--------------------------------------------------------------------------------------------------
-__attribute__((unused)) static le_msg_SessionRef_t GetCurrentSessionRef
+le_msg_SessionRef_t _{{apiName}}_GetCurrentSessionRef
 (
     void
 )
@@ -507,7 +507,7 @@ void {{apiName}}_DisconnectService
 //--------------------------------------------------------------------------------------------------
 {{function.comment|FormatHeaderComment}}
 //--------------------------------------------------------------------------------------------------
-{{function.returnType|FormatType}} {{apiName}}_{{function.name}}
+LE_DEFINE_INLINE {{function.returnType|FormatType}} {{apiName}}_{{function.name}}
 (
     {%- for parameter in function|CAPIParameters %}
     {{parameter|FormatParameter}}{% if not loop.last %},{% endif %}
@@ -516,13 +516,5 @@ void {{apiName}}_DisconnectService
     {%-else%}
     void
     {%-endfor%}
-)
-{
-    {% if function.returnType %}return{% endif %} ifgen_{{apiBaseName}}_{{function.name}}(
-        GetCurrentSessionRef()
-        {%- for parameter in function|CAPIParameters %}{% if loop.first %},{% endif %}
-        {{parameter|FormatParameterName}}{% if not loop.last %},{% endif %}
-        {%- endfor %}
-    );
-}
+);
 {%- endfor %}
